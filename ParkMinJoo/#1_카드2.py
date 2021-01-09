c1841d600b62abf6279dc6969845db6602cb11f5
MinJoo-Park01 <supermp@hufs.ac.kr>
2021-01-10 00:37:58 +0900

    [BOJ] #2164 sol2

--- ParkMinJoo/#1_카드2.py
+++ ParkMinJoo/#1_카드2.py
@@ -1,4 +1,20 @@
 # 백준 2164
+
+from collections import deque
+
+
+def card(DQ):
+    while(len(DQ) != 1):
+        DQ.popleft()
+        DQ.append(DQ.popleft())
+    return DQ[0]
+
+
+n = int(input())
+deque = deque([i for i in range(1, n+1)])
+print(card(deque))
+
+'''
 # 답은 나오는데...시간초과라고 뜬다....
 def card(A):
     n = len(A)
@@ -19,3 +35,4 @@ for i in range(n):
     queue.append(i+1)
 Answer = card(queue)
 print(Answer[0])
+'''
